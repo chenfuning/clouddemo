b246f6dd1a69ec172b7de94c4b292e4778f6d23e
ning <1097984277@qq.com>
2023-02-06 17:26:36 +0800

    nacos discover

---
+++ springcloud-nacos-client-7150/src/main/java/com/ning/nProvider/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.ning.nProvider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping(value = "/echo/{string}")
+    public String echo(@PathVariable String string) {
+        return "Hello Nacos Discovery " + string;
+    }
+}
